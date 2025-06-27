67731e306651167c71788766720584bf2137bdc7
Camilo Garzon Moreno <Camilo96327@hotmail.com>
2025-06-27 14:37:35 -0500

    Se carga todo lo de los diccionarios de las encuesta

---
+++ scripts/06_descargar_diccionario_694.py
@@ -0,0 +1,23 @@
+import os
+import json
+import requests
+
+file = '../Inkadata/diccionarios/694'
+os.makedirs(file, exist_ok=True)
+
+for i in range(5190, 5580):
+    url = f'https://microdatos.dane.gov.co/index.php/metadata/export_variable/694/V{i}/json'
+    response = requests.get(url)
+    if response.status_code ==200:
+        print(f'📡 Conexion satisfactoria, {response.status_code}')
+    else:
+        print(f'✖️ NO hay conexion en {url}, el codigo es: {response.status_code}')
+
+    print(f'⬇️ Inicia descarga del diccionario v{i}')
+    data = response.json()
+    name = data['name']
+
+    with open(f'{file}/{name}.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+    print(f'✅ Archivo {name}, guardado con exito!')
